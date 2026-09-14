6693b88a091e9626d540f419b7ccda03281dde7d
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-09-15 00:03:12 +0100

    Bckend Configuration Succesfully

---
+++ app.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from 'express';
+import {pool} from './config/database.js';
+
+const connection = await pool.getConnection()
+const app = express()
+app.set("view engine","ejs");
+app.set('views','views')
+app.use(express.static("public"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>{
+    res.render('home')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});app.use((req,res) => {
+    res.status(404).send('page not found')
+})
