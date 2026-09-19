30ba4b22212bbb13187e6046a3c5d35a353e6657
krrsalmane <karroumsalmane@gmail.com>
2026-09-19 16:30:12 +0100

    Add account service functions for retrieving client accounts

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,3 +1,10 @@
+import {
+    getClientAccounts,
+    getAccountById
+}from '../services/account.service.js'
+
+
+
 export function showClientDashboard( req , res ){
     res.render('client/dashboard', {
         user: req.session.user
---
+++ services/account.service.js
@@ -0,0 +1,22 @@
+import pool from '../config/database.js'
+
+export async function getClientAccounts(userId) {
+    const [rows] = await pool.query(
+        `SELECT *
+        FROM bank_accounts
+        WHERE    user_id = ?` ,
+        [userId]
+    );
+    return rows;
+}
+
+export async function getAccountById(accountId , userId){
+    const [rows] = await pool.query(
+        `SELECT *
+        FROM bank_accounts
+        WHERE id = ? `,
+        [accountId ,userId]
+
+    );
+    return rows[0]
+}
